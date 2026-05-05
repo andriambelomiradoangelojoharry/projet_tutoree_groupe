fc593e573291429d098dd6a87d99973b30e61c41
Sitraka <razafindrakotositrakiniaina@gmail.com>
2026-05-05 12:30:36 +0300

    Commit before merging dev/livres to main

--- livres/urls.py
+++ livres/urls.py
@@ -5,7 +5,7 @@ from .import views
 urlpatterns = [
     path('', views.liste_livre , name="liste_livre"),#le liste any @ views
     path('ajouter/', views.ajouter_livre , name="ajouter_livre"),
-    path('recherche/', views.recherche, name='recherche'),
+    path('recherche/', views.recherche, name='rechercheLivre'),
     path("modifier/<str:ref>/", views.modifier_livres, name="modifierLivre"),
     path("supprimer/<str:ref>/", views.supprimer_livres, name="supprimerLivre"),
 ]
--- livres/views.py
+++ livres/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render , redirect
 from .models import Livre
 from .forms import LivreForm
+from django.db.models import Q
 
 def liste_livre(request) :
     livres = Livre.objects.all()
@@ -16,12 +17,19 @@ def ajouter_livre(request):
     return render(request , 'livres/formulaire.html' , {'form' : form})
 
 def recherche(request) :
-    query = request.GET.get('livres')
-    livres =  Livre.objects.filter(titre__icontains=query)
-    context = {
-        'livres' : livres
-    }
-    #return render (request, 'resultat_recherche.html', context)
+    query = request.GET.get('q','').strip()
+    if not query:
+        return redirect('liste_livre')
+    else:
+        nbre_resultat = 0
+        livre_lookup = Q(reference__icontains=query) | Q(titre__icontains=query) | Q(auteur__icontains=query) 
+        livres =  Livre.objects.filter(livre_lookup)
+        context = {
+            'livres' : livres,
+            'nbre_resultat' : livres.count(),
+            'query' : query
+        }
+        return render (request, 'livres/list.html', context)
 
 
 def modifier_livres(request , ref) :
