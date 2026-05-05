0331a5360606531cb0dbd3ee6c0af312361cb92c
Sitraka <razafindrakotositrakiniaina@gmail.com>
2026-05-05 11:05:48 +0300

    Ajout url modification et supression livre

--- livres/urls.py
+++ livres/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.liste_livre , name="liste_livre"),#le liste any @ views
     path('ajouter/', views.ajouter_livre , name="ajouter_livre"),
     path('recherche/', views.recherche, name='recherche'),
+    path("modifier/<int:id>/", views.modifier_livres, name="modifier_livres"),
+    path("supprimer/<int:id>/", views.supprimer_livres, name="supprimer_livres"),
 ]
